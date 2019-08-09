07c6b25e34d1e787d89473be59d5e58cac478de1
kirillkomarovsky <53707447+kirillkomarovsky@users.noreply.github.com>
2019-08-09 16:01:39 +0300

    Update bot_tg.py

--- bot_tg.py
+++ bot_tg.py
@@ -7,5 +7,6 @@ bot = telebot.TeleBot("937310989:AAF7dZoQNzJ7pGt8vf84golvej2__AgUjvE")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	bot.reply_to(message, message.text)
+	bot.send_message(message.chat.id, message.text)
+	#bot.reply_to(message, message.text)
 bot.polling(none_stop = True, interval = 0)

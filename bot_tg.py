060ea7410b3e428bb7f04ab6fb0d607acb8bf8ff
kirillkomarovsky <53707447+kirillkomarovsky@users.noreply.github.com>
2019-08-09 13:29:39 +0300

    Add files via upload

---
+++ bot_tg.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+token = os.getenv("937310989:AAF7dZoQNzJ7pGt8vf84golvej2__AgUjvE%")
+bot = telebot.TeleBot(token)
+bot = telebot.TeleBot("937310989:AAF7dZoQNzJ7pGt8vf84golvej2__AgUjvE")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.reply_to(message, message.text)
+bot.polling(none_stop = True, interval = 0)
